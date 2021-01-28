eaf8d3624dd36bbce06dcb53b036ff3f3bba3f9d
r08922129 <r08922129@csie.ntu.edu.tw>
2021-01-28 12:30:13 +0800

    downsize corpus

--- src/QABot.py
+++ src/QABot.py
@@ -15,9 +15,9 @@ import os
 
 class QABot(object):
     def __init__(self, corpus_path):
-        with open(os.path.join(corpus_path, 'qa_pairs.pkl'), "rb") as f1,              open(os.path.join(corpus_path, 'corpus.pkl'), "rb") as f2,              open(os.path.join(corpus_path, 'dictionary.pkl'), "rb") as f3,              open(os.path.join(corpus_path, 'stopwords.txt')) as f4:
+        with open(os.path.join(corpus_path, 'answers.pkl'), "rb") as f1,              open(os.path.join(corpus_path, 'corpus.pkl'), "rb") as f2,              open(os.path.join(corpus_path, 'dictionary.pkl'), "rb") as f3,              open(os.path.join(corpus_path, 'stopwords.txt')) as f4:
 
-            self.qa_pairs = pickle.load(f1)
+            self.answers = pickle.load(f1)
             self.corpus = pickle.load(f2)
             self.dictionary = pickle.load(f3)
             self.stopwords = set()
@@ -36,6 +36,6 @@ class QABot(object):
         new_vec_tfidf = self.tfidf[new_vec]
         sims = np.array(self.index[new_vec_tfidf])
 
-        answer = self.qa_pairs[sims.argmax()][1]
+        answer = self.answers[sims.argmax()][1]
         return answer
 
